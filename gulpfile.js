da371e7e13258059e54df9685118cd6e92eea204
Kamil Waheed <kamilwaheed@icloud.com>
2016-02-20 18:44:25 +0500

    updated gulpfile to run server, and build client js + email templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,28 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var prefix = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const path = require('path');
+const fs = require('fs');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const prefix = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const shell = require('gulp-shell');
+const webpack = require('gulp-webpack');
+const forever = require('forever-monitor');
 
-var paths = {
-  scss: 'styles/scss/**/*.scss',
-  css: 'styles/css/**/*.css',
-  scssRoot: 'styles/scss/',
-  cssRoot: 'styles/css/',
-  html: './**/*.html'
+const paths = {
+  scss: 'static/styles/scss/**/*.scss',
+  css: 'static/styles/css/**/*.css',
+  scssRoot: 'static/styles/scss/',
+  cssRoot: 'static/styles/css/',
+  html: './**/*.hbs',
+  jsEntry: 'static/js/src/main.js',
+  jsOut: 'static/js/dist/'
 };
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['sass'], function () {
   browserSync.init({
-    server: {
-      baseDir: './'
-    }
+    proxy: 'http://localhost:8000',
+    serverStatic: ['.']
   });
 
   gulp.watch(paths.scss, ['sass']);
@@ -43,12 +49,56 @@ function compileSass(browserSyncStream, compress) {
   return result;
 }
 
-gulp.task('sass', function() {
+function compileWebpack(dev) {
+  const webpackConfig = require('./webpack.config.js');
+  return gulp.src(paths.jsEntry)
+    .pipe(webpack(dev ? webpackConfig.dev : webpackConfig.prod, null, function (err, stats) {
+      const main = Object.keys(stats.compilation.assets)[0];
+      const file = JSON.stringify({ scripts: [ main ] });
+
+      fs.writeFile('./webpack-stats.json', file, (err) => {
+        if (err) {
+          console.warn('couldn\'t write webpack stats file');
+        }
+
+        console.log('webpack stats file written');
+      });
+    }))
+    .pipe(gulp.dest(paths.jsOut));
+};
+
+
+gulp.task('sass', function () {
   return compileSass(true);
 });
 
-gulp.task('dev', ['serve']);
+gulp.task('app', function () {
+  const child = new (forever.Monitor)('index.js', {
+    env: { DEBUG: '*' }
+  });
+
+  child.on('exit', function () {
+    console.log('index.js has exited');
+  });
+
+  child.start();
+});
+
+gulp.task('email-templates', shell.task([
+  'node email/build'
+]));
+
+gulp.task('clientjs', function () {
+  compileWebpack(true);
+})
+
+gulp.task('clientjs-prod', function () {
+  compileWebpack(false);
+});
+
+gulp.task('server', ['app']);
+gulp.task('dev', ['email-templates', 'clientjs', 'server', 'serve']);
 gulp.task('default', ['dev']);
-gulp.task('build', function() {
+gulp.task('build', ['email-templates', 'clientjs-prod'], function () {
   return compileSass(false, true);
 });
